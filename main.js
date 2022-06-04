df7d2a3106d8a897686622301129e563be50bf74
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 19:24:23 +0200

    add event listener and changeColor function

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 let red = 100;
 let green = 100;
 let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = () => {
+    console.log("klawisz");
+
+}
+
+window.addEventListener('keydown', changeColor)
