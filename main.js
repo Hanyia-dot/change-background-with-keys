22d6a295b33287b5e4b359ff87ecb2831613b6bd
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 20:07:13 +0200

    add two ways IF and SWITCH statment

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
-const changeColor = () => {
-    console.log("klawisz");
+const changeColor = (e) => {
+    //console.log(e.keyCode);   - CHECK THE KEY NUMBER  
+
+    /* const keyUp = e.keyCode == 38;
+    co nst keyDown = e.keyCode == 40*/
+
+    /* //   IF,  ELSE IF
+    if(keyUp){
+        document.body.style.backgroundColor = `rgb(${red++}, ${green++}, ${blue++})`;
+    } 
+    //else if
+    if (keyDown){
+        document.body.style.backgroundColor = `rgb(${red--}, ${green--}, ${blue--})`;
+    } */
+    
+    //   SWITCH
+
+    const key = e.keyCode;
+    switch(key) {
+        case 38:
+            red = red + 2;
+            green = green + 2;
+            blue = blue + 2;
+            document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            break;
+
+        case 40:
+            red = red - 2;
+            green = green - 2;
+            blue = blue - 2;
+            document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            //break;
+    }
+
+
+
 
 }
 
